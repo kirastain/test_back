6a44b45532acb4d0a2a603d39eaea9ad56b0e5bc
Daria Fedorova <36547686+kirastain@users.noreply.github.com>
2020-05-16 16:06:50 +0300

    Revert "json format"
    This reverts commit 092c457330c9ff26b6852bd8c1f0256349f272a6.

--- main.php
+++ main.php
@@ -9,12 +9,15 @@ class       DataNode {
 
 function    PrintTable($TestDB)
 {
-    $table = $TestDB->query('SELECT * FROM maindata'); //PDOStatement
+    $table = $TestDB->query('SELECT id, main_data, parent_id, create_date FROM maindata'); //PDOStatement
     if ($table)
     {
         foreach ($table as $row)
         {
-            print json_encode($row, JSON_PRETTY_PRINT);
+            print $row['id'] . '\t';
+            print $row['main_data'] . '\t';
+            print $row['parent_id'] . '\t';
+            print $row['create_date'] . '\t';
         }
     }
 }
@@ -26,17 +29,16 @@ function    PrintTableId($TestDB, $CurrentId)
     {
         foreach ($line as $row)
         {
-            /*print $row['id'] . '\t';
+            print $row['id'] . '\t';
             print $row['main_data'] . '\t';
             print $row['parent_id'] . '\t';
-            print $row['create_date'] . '\t';*/
-            print json_encode($row, JSON_PRETTY_PRINT);
+            print $row['create_date'] . '\t';
         }
     }
 }
 
 $myDB = new PDO('pgsql:host=localhost;dbname=testdb', 'postgres', '1410');
-PrintTable($myDB);
-//PrintTableId($myDB, 1);
+//PrintTable($myDB);
+PrintTableId($myDB, 1);
 
 
