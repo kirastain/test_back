2c79bc6fae59eb223157b0e181a07e6e6ceae03a
Daria Fedorova <36547686+kirastain@users.noreply.github.com>
2020-05-16 18:40:29 +0300

    partial edit

--- main.php
+++ main.php
@@ -1,26 +1,24 @@
 <?php
 
-/*class       DataNode {
+/*class DataNode {
     //public $Id; //look up if it's needed or it's created automatically
     public  $mainData;
     public  $parentId;
     public  $createDate;
 } */
 
-function    printTable($testDB)
+function printTable($testDB): void
 {
     $table = $testDB->query('SELECT * FROM maindata'); //PDOStatement
     if ($table) {
         $result = $table->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($result as $row) {
-            print(json_encode($row, JSON_PRETTY_PRINT));
-        }
+        print(json_encode($result, JSON_PRETTY_PRINT));
     } else {
         print("Table not found\n");
     }
 }
 
-function    printTableId($testDB, $currentId)
+function printById($testDB, int $currentId): void
 {
     $line = $testDB->query("SELECT * FROM maindata WHERE id=$currentId");
     if ($line) {
@@ -31,17 +29,17 @@ function    printTableId($testDB, $currentId)
     }
 }
 
-function    updateTableId($testDB, $currentId, $updData)
+function updateById($testDB, int $currentId, string $updData): void
 {
     $upd = $testDB->query("UPDATE maindata SET main_data='$updData' WHERE id=$currentId");
     if ($upd) {
-        printTableId($testDB, $currentId);
+        printById($testDB, $currentId);
     } else {
         print("Error updating data\n");
     }
 }
 
 $myDB = new PDO('pgsql:host=localhost;dbname=testdb', 'postgres', '1410');
-//PrintTable($myDB); //bad getaway when trying to view in browser
-//PrintTableId($myDB, 1);
-UpdateTableId($myDB, 2, "edited data");
+PrintTable($myDB); //bad getaway when trying to view in browser
+//PrintById($myDB, 1);
+//UpdateById($myDB, 1, "edited");
