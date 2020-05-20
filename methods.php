4b7ca2b77b314757308a35d67d84b00549fb4f79
Daria Fedorova <36547686+kirastain@users.noreply.github.com>
2020-05-20 11:22:41 +0300

    prepare execute

--- methods.php
+++ methods.php
@@ -18,14 +18,14 @@ function printTable(): void
     $myDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $myDb->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     try {
-        $table = $myDb->query('SELECT * FROM maindata'); //PDOStatement
+        $sql = "SELECT * FROM maindata";
+        $table = $myDb->prepare($sql);
+        $table->execute();
         $result = $table->fetchAll(PDO::FETCH_ASSOC);
         print(json_encode($result, JSON_PRETTY_PRINT));
     } catch (PDOException $e) {
         var_dump($e->getMessage());
     }
-    //$result = $table->fetchAll(PDO::FETCH_ASSOC);
-    //print(json_encode($result, JSON_PRETTY_PRINT));
 }
 
 /**
@@ -38,7 +38,9 @@ function printById(int $currentId): void
     $myDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $myDb->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     try {
-        $line = $myDb->query("SELECT * FROM maindata WHERE id=$currentId");
+        $sql = "SELECT * FROM maindata WHERE id = :currentId";
+        $line = $myDb->prepare($sql);
+        $line->execute(array(':currentId' => $currentId));
         $result = $line->fetch(PDO::FETCH_ASSOC);
         print(json_encode($result, JSON_PRETTY_PRINT));
     } catch (Exception $e) {
@@ -57,7 +59,9 @@ function updateById(int $currentId, string $updData): void
     $myDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $myDb->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     try {
-        $upd = $myDb->query("UPDATE maindata SET main_data='$updData' WHERE id=$currentId");
+        $sql = "UPDATE maindata SET main_data = :updData WHERE id = :currentId";
+        $upd = $myDb->prepare($sql);
+        $upd->execute(array(':updData' => $updData, ':currentId' => $currentId));
         printById($currentId);
     } catch (Exception $e) {
         var_dump(($e->getMessage()));
